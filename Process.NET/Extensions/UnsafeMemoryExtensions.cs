a98ef9a79fc518282e2b30f3d6c96cf76c4e36ae
Sean Thomas <sean.daxxtrias@gmail.com>
2018-08-15 10:51:44 -0500

    Fastcall isn't support by Process.NET, adding in preliminary unfinished code

--- Process.NET/Extensions/UnsafeMemoryExtensions.cs
+++ Process.NET/Extensions/UnsafeMemoryExtensions.cs
@@ -6,6 +6,48 @@ using Process.NET.Native;
 
 namespace Process.NET.Extensions
 {
+    public static class FastCall
+    {
+        public static IntPtr InvokePtr { get; private set; }
+
+        private static Byte[] InvokeCode = new Byte[]
+            {
+            0x5A,                           // pop edx
+            0x36, 0x87, 0x54, 0x24, 0x08,   // xchg ss:[esp+08],edx
+            0x58,                           // pop eax
+            0x59,                           // pop ecx
+            0xFF, 0xE0                      // jmp eax
+            };
+
+        private static Byte[] WrapperCode = new Byte[]
+            {
+            0x58,                           // pop eax
+            0x52,                           // push edx
+            0x51,                           // push ecx
+            0x50,                           // push eax
+            0x68, 0x00, 0x00, 0x00, 0x00,   // push ...
+            0xC3                            // retn
+            };
+        /*
+        static FastCall()
+        {
+            FastCall.InvokePtr = Kernel32.VirtualAlloc(IntPtr.Zero, FastCall.InvokeCode.Length,
+                AllocationType.Commit, MemoryProtection.ExecuteReadWrite);
+
+            Marshal.Copy(FastCall.InvokeCode, 0, FastCall.InvokePtr, FastCall.InvokeCode.Length);
+        }
+
+        public static IntPtr WrapStdCallInFastCall(IntPtr stdCallPtr)
+        {
+            var result = Kernel32.VirtualAlloc(IntPtr.Zero, FastCall.WrapperCode.Length, AllocationType.Commit, MemoryProtection.ExecuteReadWrite);
+
+            Marshal.Copy(FastCall.WrapperCode, 0, result, FastCall.WrapperCode.Length);
+            Marshal.WriteIntPtr(result, 5, stdCallPtr);
+
+            return result;
+        }
+        */
+    }
     public static class UnsafeMemoryExtensions
     {
         // Gets an address from a vtable index.Since it uses index * IntPtr, it should work for both x64 and x32.
